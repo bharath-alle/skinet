43c04dad65425678af7cdeb5c22f3812851e0b9d
Bharath <Bharath.alle@gmail.com>
2022-07-09 22:20:49 +0530

    End of sec 3

---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Core.Entities;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Data
+{
+    public class StoreContextSeed
+    {
+        public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory)
+        {
+            var logger = loggerFactory.CreateLogger<StoreContextSeed>();
+            try
+            {
+                var bb = context.ProductBrands.ToList();
+                if (context.ProductBrands.Count()<=0)
+                {
+                    var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+                    var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+                    foreach (var item in brands)
+                    {
+                        context.ProductBrands.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+
+                if (context.ProductTypes.Count()<=0)
+                {
+                    var typesData = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+                    var types = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                    foreach (var item in types)
+                    {
+                        context.ProductTypes.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+                if (context.Products.Count()<=0)
+                {
+                    var productsData = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+                    var products = JsonSerializer.Deserialize<List<Product>>(productsData);
+                    foreach (var item in products)
+                    {
+                        context.Products.Add(item);
+                    }
+
+                    await context.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, ex.Message);
+            }
+        }
+    }
+}
